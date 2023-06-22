37e0e06063580c136d437e568d203023283c15aa
thomasdao <thomas.daoxuandung@gmail.com>
2023-06-22 10:58:54 +0700

    Add div to DEFAULT_TAGS_TO_SCORE so it fixes the issue with content being cutoff from NewYorker article

--- Readability.js
+++ Readability.js
@@ -112,7 +112,7 @@ Readability.prototype = {
   DEFAULT_N_TOP_CANDIDATES: 5,
 
   // Element tags to score by default.
-  DEFAULT_TAGS_TO_SCORE: "section,h2,h3,h4,h5,h6,p,td,pre".toUpperCase().split(","),
+  DEFAULT_TAGS_TO_SCORE: "section,h2,h3,h4,h5,h6,p,td,pre,div".toUpperCase().split(","),
 
   // The default number of chars an article must have in order to return a result
   DEFAULT_CHAR_THRESHOLD: 500,
